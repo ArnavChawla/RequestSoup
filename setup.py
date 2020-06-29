270c82e590a0e13f48affb9e9849f3fd4a402f7e
Arnav Chawla <arnavchawla23@gmail.com>
2020-06-29 15:28:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
-
+with open("README.MD","r") as fh:
+    long = fh.read()
 setup(
     name = 'RequestSoup',
-    version='0.0.1',
+    version='0.0.2',
     description='A wrapper created to make using requests and BeautifulSoup in conjunction easier',
+    long_description=long,
+    long_description_content_type="text/markdown",
     py_modules=["RequestSoup"],
     package_dir={'':'src'},
     install_requires = [
